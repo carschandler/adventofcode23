8398551d34228039605a315f190eadaeb958b179
Cars Chandler <carschandler7@gmail.com>
2023-12-23 23:48:45 -0600

    Complete day 2

--- src/bin/d2p1.rs
+++ src/bin/d2p1.rs
@@ -1,32 +1,31 @@
-fn main () {
+fn main() {
     println!("{}", calc("./input/day2/input.txt"));
 }
 
 fn calc(input_file: &str) -> i32 {
-    let num_cubes = std::collections::HashMap::from([
-        ("red", 12),
-        ("green", 13),
-        ("blue", 14),
-    ]);
-
-    let mut total_sum: i32 = (1..=100).into_iter().reduce(|acc, e| acc + e).unwrap();
-
-    dbg!(total_sum);
+    let mut sum = 0;
 
     let input = std::fs::read_to_string(input_file).unwrap();
-    for (id, line) in input.lines().enumerate() {
+
+    for (_id, line) in input.lines().enumerate() {
+        let mut min_num_cubes =
+            std::collections::HashMap::from([("red", 0), ("green", 0), ("blue", 0)]);
+
         let game = line.split_once(": ").unwrap().1;
-        'line: for set in game.split("; ") {
+
+        for set in game.split("; ") {
             for draw in set.split(", ") {
                 let (num, color) = draw.split_once(' ').unwrap();
                 let num = str::parse::<i32>(num).unwrap();
-                if num > *num_cubes.get(color).unwrap() {
-                    total_sum -= id as i32 + 1;
-                    break 'line;
+                if num > *min_num_cubes.get(color).unwrap() {
+                    min_num_cubes.insert(color, num);
                 }
             }
-        };
+        }
+        // In theory, we don't even need the unwrap_or here since the reduced value should be zero
+        // even if no "sets" were drawn
+        sum += min_num_cubes.into_values().reduce(|acc, e| acc * e).unwrap_or(0);
     }
 
-    return total_sum;
+    return sum;
 }
